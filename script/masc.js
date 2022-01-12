204dcfd1e58a2d0a2b65c1f8ef0ac38e0f76d7bd
MarcosF19 <marcos.lima52@fatec.sp.gov.br>
2022-01-11 22:24:19 -0300

    JS em inicio

--- script/masc.js
+++ script/masc.js
@@ -1,3 +1,17 @@
-function calc() {
+function enviar() {
+
+    var nome = window.document.getElementById('txtnome') 
+
+    var res = window.document.querySelector('section#res')
+    if(nome.value.lenght == 0 ) {
+        window.alert('ERRO - Campo vazio')
+    }
+    else {
+        window.alert(`Ok ${nam} vamos entrar em contato!`)
+    }
+
+    res.innerHTML = `Ok ${nome} vamos entrar em contato assim que puder!`
     
 }
+
+
